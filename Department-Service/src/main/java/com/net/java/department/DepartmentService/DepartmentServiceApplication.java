da9e68b86a38103d79f4fe3b7c7d77d657639843
Aniket Jawanjal <aniketjawanjal01@gmail.com>
2024-11-07 23:42:30 +0530

    depertment service

--- Department-Service/src/main/java/com/net/java/department/DepartmentService/DepartmentServiceApplication.java
+++ Department-Service/src/main/java/com/net/java/department/DepartmentService/DepartmentServiceApplication.java
@@ -3,10 +3,12 @@ package com.net.java.department.DepartmentService;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
